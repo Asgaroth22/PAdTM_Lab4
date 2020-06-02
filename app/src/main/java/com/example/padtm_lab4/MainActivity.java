43e222112e686b42c665351639db927e092cd0c6
Bartek <iluvatar.er@gmail.com>
2020-06-02 20:09:38 +0200

    Usuwanie wpisów

--- app/src/main/java/com/example/padtm_lab4/MainActivity.java
+++ app/src/main/java/com/example/padtm_lab4/MainActivity.java
@@ -53,7 +53,16 @@ public class MainActivity extends AppCompatActivity {
             startActivityForResult(intencja, 2);
         }
     });
-
+        listview.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView name = (TextView) view.findViewById(android.R.id.text1);
+                db.usun(name.getText().toString());
+                adapter.changeCursor(db.lista());
+                adapter.notifyDataSetChanged();
+                return false;
+            }
+        });
     }
 
     @Override
