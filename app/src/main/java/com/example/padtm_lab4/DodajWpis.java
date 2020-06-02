55dd327f9a1db862df40e3563d718a2680ddd126
Bartek <iluvatar.er@gmail.com>
2020-06-02 20:02:57 +0200

    Edytowanie wpisów

--- app/src/main/java/com/example/padtm_lab4/DodajWpis.java
+++ app/src/main/java/com/example/padtm_lab4/DodajWpis.java
@@ -11,6 +11,7 @@ import android.widget.Spinner;
 
 public class DodajWpis extends AppCompatActivity {
 
+    private int modified_id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,9 +19,31 @@ public class DodajWpis extends AppCompatActivity {
         ArrayAdapter gatunki = new ArrayAdapter(
                 this,
                 android.R.layout.simple_spinner_dropdown_item,
-                new String[] {"Pies", "Kot", "Rybki"});
+                new String[]{"Pies", "Kot", "Rybki"});
         Spinner gatunek = (Spinner) findViewById(R.id.editGatunek);
         gatunek.setAdapter(gatunki);
+
+        Bundle extras = getIntent().getExtras();
+        try {
+            if (extras.getSerializable("element") !=
+                    null) {
+                Animal zwierz = (Animal)
+                        extras.getSerializable("element");
+                EditText kolor = (EditText)
+                        findViewById(R.id.editKolor);
+                EditText wielkosc = (EditText)
+                        findViewById(R.id.editWielkosc);
+                EditText opis = (EditText)
+                        findViewById(R.id.editOpis);
+                kolor.setText(zwierz.getKolor());
+                wielkosc.setText(
+                        Float.toString(zwierz.getWielkosc()));
+                opis.setText(zwierz.getOpis());
+                this.modified_id = zwierz.getId();
+            }
+        } catch (Exception ex) {
+            this.modified_id = 0;
+        }
     }
 
     public void wyslij (View view)
@@ -35,6 +58,7 @@ public class DodajWpis extends AppCompatActivity {
                 Float.valueOf(wielkosc.getText().toString()),
                 opis.getText().toString()
         );
+        zwierze.setId(this.modified_id);
         Intent intencja = new Intent();
         intencja.putExtra("nowy", zwierze);
         setResult(RESULT_OK, intencja);
