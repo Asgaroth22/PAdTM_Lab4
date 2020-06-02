5db971f1f7cd469ec836234b76cd0b32fc8442ee
Bartek <iluvatar.er@gmail.com>
2020-06-02 18:55:26 +0200

    Tworzenie bazy danych i metod CRUD

---
+++ app/src/main/java/com/example/padtm_lab4/Animal.java
@@ -0,0 +1,64 @@
+package com.example.padtm_lab4;
+
+import java.io.Serializable;
+
+public class Animal implements Serializable {
+    private int id;
+    private String gatunek;
+    private String kolor;
+    private float wielkosc;
+    private String opis;
+    public Animal(){}
+    public Animal(String gatunek, String kolor, float wielkosc, String opis){
+        super();
+        this.gatunek=gatunek;
+        this.kolor=kolor;
+        this.wielkosc=wielkosc;
+        this.opis=opis;
+    }
+
+    @Override
+    public String toString(){
+        return "Zwierze: [id=" + id + ", gatunek=" + gatunek + ", kolor=" + kolor + ", wielkosc=" + wielkosc + " ]";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getGatunek() {
+        return gatunek;
+    }
+
+    public void setGatunek(String gatunek) {
+        this.gatunek = gatunek;
+    }
+
+    public String getKolor() {
+        return kolor;
+    }
+
+    public void setKolor(String kolor) {
+        this.kolor = kolor;
+    }
+
+    public float getWielkosc() {
+        return wielkosc;
+    }
+
+    public void setWielkosc(float wielkosc) {
+        this.wielkosc = wielkosc;
+    }
+
+    public String getOpis() {
+        return opis;
+    }
+
+    public void setOpis(String opis) {
+        this.opis = opis;
+    }
+}
